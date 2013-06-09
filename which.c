819050a3d7944432ad46efee9fe802cdd6e0873a
Matt Houglum <mhoug89@gmail.com>
2013-06-08 22:44:15 -0500

    first commit, only wrote which

---
+++ which.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <malloc.h>
+
+void printHelp();
+
+int main(int argc, char** argv)
+{
+    int curIndex, i;    // loop counters
+    char *path, *pathToSplit, *pathDelim, *curFolder, curFile[2056];
+    struct stat curStat;
+
+    // print a help section if the user only enters "which"
+    if (argc == 1) {
+        printHelp();
+        return 0;
+    }
+
+    // retreive the user's PATH variable to look in
+    path = getenv("PATH");
+    pathToSplit = (char*)malloc(sizeof(char) * (strlen(path)+1));
+    if (path == NULL) {
+        printf("You don't seem to have a PATH variable defined.\n");
+        return 0;
+    }
+
+    // for each argument provided to which, figure out if it exists
+    // and in which folder in the user's PATH it does; if an argument
+    // does not exist, don't print it
+    for (curIndex = 1; curIndex < argc; curIndex++) {
+        // don't worry about clearing the pathToSplit buffer -- it is only
+        // truncated after the next line
+        strncpy(pathToSplit, path, strlen(path));
+        // clear out contents of old file name
+        memset(curFile, 0, strlen(curFile));
+
+        // if the user specifies an absolute or relative path,
+        // search for the file and print it out if it exists.
+        if (argv[curIndex][0] == '/') {
+            if (stat(argv[curIndex], &curStat) == 0) {
+	            if (S_ISREG(curStat.st_mode)) {
+	            	printf("%s\n", argv[curIndex]);
+	            }
+	        }
+	    }
+	    else if (argv[curIndex][0] == '.') {
+	        // prepend the relative path and "/" before checking
+	        getcwd(curFile, 2056);
+            strncat(curFile, "/", 1);
+            strncat(curFile, argv[curIndex], strlen(argv[curIndex]));
+	        if (stat(curFile, &curStat) == 0) {
+                printf("%s\n", curFile);
+	        }
+	    }
+
+        // doesn't have a prepended path, so search each folder in the PATH
+        // variable, one at a time; stop at the first instance you find
+        else {
+            curFolder = strtok(pathToSplit, ":");
+
+            while (curFolder != NULL) {
+                memset(curFile, 0, strlen(curFile));
+                // curFile will become:
+                // [folder in PATH] + "/" + [argument passed to which]
+                strncpy(curFile, curFolder, strlen(curFolder));
+                strncat(curFile, "/", 1);
+                strncat(curFile, argv[curIndex], strlen(argv[curIndex]));
+                if (stat(curFile, &curStat) == 0) {
+                    printf("%s\n", curFile);
+                    break;
+    	        }
+
+                curFolder = strtok(NULL, ":");
+            }
+
+        }
+    }
+
+    return 0;
+}
+
+
+void printHelp() {
+    printf("Help section goes here.\n");
+}
