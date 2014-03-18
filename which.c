90c87b931418f0d42777bed1c5ff62662cf2044c
Matt Houglum <mhoug89@gmail.com>
2014-03-18 02:58:36 -0500

    Changed naming convention away from camel case

--- which.c
+++ which.c
@@ -8,23 +8,23 @@
 
 #define BUF_SZ 2056
 
-void printHelp();
+void print_help();
 
 int main(int argc, char** argv)
 {
-    int curIndex;    // loop counter to traverse arguments
-    char *path, *pathToSplit, *pathDelim, *curFolder, curFile[BUF_SZ];
-    struct stat curStat;
+    int cur_index;    // loop counter to traverse arguments
+    char *path, *path_to_split, *path_delim, *cur_folder, cur_file[BUF_SZ];
+    struct stat cur_stat;
 
     // print a help section if the user only enters "which"
     if (argc == 1) {
-        printHelp();
+        print_help();
         return 0;
     }
 
     // retreive the user's PATH variable to look in
     path = getenv("PATH");
-    pathToSplit = (char*)malloc(sizeof(char) * (strlen(path)+1));
+    path_to_split = (char*)malloc(sizeof(char) * (strlen(path)+1));
     if (path == NULL) {
         printf("You don't seem to have a PATH variable defined.\n");
         return 0;
@@ -33,49 +33,47 @@ int main(int argc, char** argv)
     // for each argument provided to which, figure out if it exists
     // and in which folder in the user's PATH it does; if an argument
     // does not exist, don't print it
-    for (curIndex = 1; curIndex < argc; curIndex++) {
-        // don't worry about clearing the pathToSplit buffer -- it is only
+    for (cur_index = 1; cur_index < argc; cur_index++) {
+        // don't worry about clearing the path_to_split buffer -- it is only
         // truncated after the next line
-        strncpy(pathToSplit, path, strlen(path)+1);
+        strncpy(path_to_split, path, strlen(path)+1);
         // clear out contents of old file name
-        memset(curFile, 0, BUF_SZ);
+        memset(cur_file, 0, BUF_SZ);
 
         // if the user specifies an absolute or relative path,
         // search for the file and print it out if it exists.
-        if (argv[curIndex][0] == '/') {
-            if (stat(argv[curIndex], &curStat) == 0) {
-                if (S_ISREG(curStat.st_mode)) {
-                    printf("%s\n", argv[curIndex]);
+        if (argv[cur_index][0] == '/') {
+            if (stat(argv[cur_index], &cur_stat) == 0) {
+                if (S_ISREG(cur_stat.st_mode)) {
+                    printf("%s\n", argv[cur_index]);
                 }
             }
-        }
-        else if (argv[curIndex][0] == '.') {
+        } else if (argv[cur_index][0] == '.') {
             // prepend the relative path and "/" before checking
-            getcwd(curFile, BUF_SZ);
-            strncat(curFile, "/", 1);
-            strncat(curFile, argv[curIndex], strlen(argv[curIndex]));
-            if (stat(curFile, &curStat) == 0) {
-                printf("%s\n", curFile);
+            getcwd(cur_file, BUF_SZ);
+            strncat(cur_file, "/", 1);
+            strncat(cur_file, argv[cur_index], strlen(argv[cur_index]));
+            if (stat(cur_file, &cur_stat) == 0) {
+                printf("%s\n", cur_file);
             }
-        }
-        // doesn't have a prepended path, so search each folder in the PATH
-        // variable, one at a time; stop at the first instance you find
-        else {
-            curFolder = strtok(pathToSplit, ":");
+        }  else {
+            // doesn't have a prepended path, so search each folder in the PATH
+            // variable, one at a time; stop at the first instance you find
+            cur_folder = strtok(path_to_split, ":");
 
-            while (curFolder != NULL) {
-                memset(curFile, 0, BUF_SZ);
-                // curFile will become:
+            while (cur_folder != NULL) {
+                memset(cur_file, 0, BUF_SZ);
+                // cur_file will become:
                 // [folder in PATH] + "/" + [argument passed to which]
-                strncpy(curFile, curFolder, strlen(curFolder)+1);
-                strncat(curFile, "/", 1);
-                strncat(curFile, argv[curIndex], strlen(argv[curIndex]));
-                if (stat(curFile, &curStat) == 0) {
-                    printf("%s\n", curFile);
+                strncpy(cur_file, cur_folder, strlen(cur_folder)+1);
+                strncat(cur_file, "/", 1);
+                strncat(cur_file, argv[cur_index], strlen(argv[cur_index]));
+                if (stat(cur_file, &cur_stat) == 0) {
+                    printf("%s\n", cur_file);
                     break;
                 }
 
-                curFolder = strtok(NULL, ":");
+                cur_folder = strtok(NULL, ":");
             }
 
         }
@@ -85,6 +83,6 @@ int main(int argc, char** argv)
 }
 
 
-void printHelp() {
-    printf("Help section goes here.\n");
+void print_help() {
+    printf("Help section goes here. No nifty flags can be used at the moment. \n");
 }
